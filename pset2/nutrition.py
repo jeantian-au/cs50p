d120f251107dacb136745847ff074c5feaf9fc5d
Jean Tian <jean.tian.tech@gmail.com>
2026-05-28 11:33:35 +1000

    fix(pset2): complete nutrition + fix camel/coke output format

---
+++ pset2/nutrition.py
@@ -0,0 +1,32 @@
+
+fruits_dict = {
+        "apple": 130,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 60,
+        "honeydew melon": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
+}
+
+
+def main():
+    search_fruit = input("Item: ").lower()
+
+    if search_fruit in fruits_dict:
+        print(f"Calories: {fruits_dict[search_fruit]}")
+
+main()
